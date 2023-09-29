672c5ef704e60aab57dbed015ae7714b005b43cc
ericsubirana <ericsubirana4444@gmail.com>
2023-09-29 11:29:30 +0200

    preparació2

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({"users":["user1", "user2"]})
+});
+
+app.listen(5000, () => console.log("server started on port 5000"));
+
