45c1523dd5f8aa4dd0433fc9f32d53b034a7643c
= <113594121+ericsubirana@users.noreply.github.com>
2023-09-29 14:31:52 +0200

    comunicació front i backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(morgan('dev')); //anar imprimint els resultats
+app.use(cors()); // Enable CORS for all routes
 
 app.get("/api", (req, res) => {
     res.json({"users":["user1", "user2"]})
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fuunc = async () => {
+      const response = await axios.get('http://localhost:5000/api');
+      setData(response.data);
+      console.log(response.data);
+    };
+    fuunc();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? data.users.map(user=> <div  key={user} >{user}</div>) : "Loading..." }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
