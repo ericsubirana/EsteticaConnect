8a68075092ca31837f6e27085ceac300389c61cd
= <113594121+ericsubirana@users.noreply.github.com>
2023-10-10 11:06:22 +0200

    header v3 & login started

--- front/src/components/header/Header.js
+++ front/src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import img from '../../assets/f.png';
 import Sidebar from '../sidebar/Sidebar';
 import Searchbar from '../searchbar/Searchbar';
 import LogInLogOut from '../logInLogOut/LogInLogOut';
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 export default function Login() {
+
+  const [email,setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  async function login () {
+    const user = await axios.post('http://localhost:5000/api/login', {email, password});
+    if(user){
+      
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+        <label>
+          Email:
+          <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button onClick={login}> Submit </button>
+    </div>
+  )}
