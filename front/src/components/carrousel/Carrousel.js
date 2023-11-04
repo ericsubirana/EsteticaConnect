472ea86a10c011954c7aba10d5afa3b288eec953
ericsubirana <ericsubirana4444@gmail.com>
2023-11-04 10:49:28 +0100

    first screen home

---
+++ front/src/components/carrousel/Carrousel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./carrousel.css"
+
+function Carrousel() {
+  return (
+    <div className='carrousel'>
+        <h1>SERVEIS</h1>
+        
+    </div>
+  )
+}
+
+export default Carrousel
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Header from '../components/header/Header'
 
 import "./home.css";
+import Carrousel from '../components/carrousel/Carrousel';
 
 function Home() {
   return (
-    <div className='homePage'>
+    <>
+      <div className='homePage'>
         <Header page="home" />
         <div className='text'>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit sociis quis auctor tempus, in lobortis sollicitudin  primis accumsan.</p>
-          <h2>Centre d'Estètica</h2>
-          <h1>Fina García</h1>
+          <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipiscing, elit sociis quis auctor tempus, in lobortis sollicitudin  primis accumsan.</p>
+          <div className='titleHome'>
+            <h2>Centre d'Estètica</h2>
+            <h1>Fina García</h1>
+          </div>
         </div>
-    </div>
+      </div>
+      <Carrousel />
+    </>
   )
 }
 
