96acac7f1671a5f6799de0a1374e8bdd7df41dcd
ericsubirana <ericsubirana4444@gmail.com>
2023-11-12 13:24:21 +0100

    login falta col.locar logo

--- front/src/components/registerComponent/RegisterForm.js
+++ front/src/components/registerComponent/RegisterForm.js
@@ -4,7 +4,7 @@ import { useAuth } from '../../context/AuthContext.js'
 import { useNavigate, Link } from 'react-router-dom';
 
 import logo from '../../assets/f.png'
-import registerPhoto from '../../assets/registerPhoto.jpg'
+import registerPhoto from '../../assets/registerPhoto.png'
 import './register.css'
 
 function RegisterForm() {
@@ -24,44 +24,54 @@ function RegisterForm() {
 
     return (
         <div className='registerFormImage'>
+            <div className='sqaure'></div>
             <div className='register'>
-            {errorContext.map((error, index) => (
-                <div key={index}>
-                    <p>{error}</p>
-                </div>
-            ))
-            }
-            <img src={logo} alt="logo" height={80} onClick={home} />
-            <h1>Registra't!</h1>
-            <form onSubmit={handleSubmit(async (values) => {
-                signup(values);
-            })}>
-                <div className='firstline'>
-                    <div className='userName'>
-                        <h3>NOM D'USUARI/A</h3>
-                        <input type="text" {...register('username', { required: true })} />
-                        {errors.username && <p>Username is required</p>}
+                {errorContext.map((error, index) => (
+                    <div key={index}>
+                        <p>{error}</p>
                     </div>
-                    <div>
-                        <h3>CONTRSENYA</h3>
-                        <input type="password" {...register('password', { required: true })} />
-                        {errors.password && <p>Password is required</p>}
+                ))
+                }
+                <img className="imgForm" src={logo} alt="logo" height={80} onClick={home} />
+                <h1>Registra't!</h1>
+                <form onSubmit={handleSubmit(async (values) => {
+                    signup(values);
+                })}>
+                    <div className='firstline'>
+                        <div className='userName'>
+                            <div>
+                                <h3>NOM D'USUARI/A</h3>
+                                {errors.username && <p>Username is required</p>}
+                            </div>
+                            <input type="text" {...register('username', { required: true })} />
+
+                        </div>
+                        <div>
+                            <div>
+                                <h3>CONTRSENYA</h3>
+                                {errors.password && <p>Password is required</p>}
+                            </div>
+                            <input type="password" {...register('password', { required: true })} />
+
+                        </div>
                     </div>
-                </div>
-                <div className='userMail'>
-                    <h3>CORREU</h3>
-                    <input type="email" {...register('email', { required: true })} />
-                    {errors.email && <p>Email is required</p>}
-                </div>
-                <button type='submit' className='button-52'>
-                    Sign Up
-                </button>
-            </form>
-            <p>Ja tens un compte? <Link to='/login'>Inicia Sessió</Link></p>
-        </div>
-        <div className='imageRegister'>
-            <img src={registerPhoto} alt='registerPhoto' height={650}/>
-        </div>
+                    <div className='userMail'>
+                        <div>
+                            <h3>CORREU</h3>
+                            {errors.email && <p>Email is required</p>}
+                        </div>
+                        <input type="email" {...register('email', { required: true })} />
+
+                    </div>
+                    <button type='submit' className='button-52'>
+                        Sign Up
+                    </button>
+                </form>
+                <p className='alreadyHasAccount'>Ja tens un compte? <Link to='/login'>Inicia Sessió</Link></p>
+            </div>
+            <div className='imageRegister'>
+                <img src={registerPhoto} alt='registerPhoto' height={500} />
+            </div>
         </div>
     )
 }
