9596837dc0fa4842bfa55801da1800fd6ddbf9f7
= <113594121+ericsubirana@users.noreply.github.com>
2023-12-12 12:45:29 +0100

    emailChanged

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -120,12 +120,12 @@ function sendEmail({ recipient_email, OTP }) {
         const mail_configs = {
             from: process.env.MY_EMAIL,
             to: recipient_email,
-            subject: "KODING 101 PASSWORD RECOVERY",
+            subject: "CE FINA PASSWORD RECOVERY",
             html: `<!DOCTYPE html>
   <html lang="en" >
   <head>
     <meta charset="UTF-8">
-    <title>CodePen - OTP Email Template</title>
+    <title>OTP Email Template</title>
     
   
   </head>
@@ -134,17 +134,17 @@ function sendEmail({ recipient_email, OTP }) {
   <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
     <div style="margin:50px auto;width:70%;padding:20px 0">
       <div style="border-bottom:1px solid #eee">
-        <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Koding 101</a>
+        <a href="" style="font-size:1.4em;color:#faafe7;text-decoration:none;font-weight:600">CE FINA</a>
       </div>
-      <p style="font-size:1.1em">Hi,</p>
-      <p>Thank you for choosing Koding 101. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
-      <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
-      <p style="font-size:0.9em;">Regards,<br />Koding 101</p>
+      <p style="font-size:1.1em">Hola!,</p>
+      <p>Gracias por elegir CE FINA. Usa el siguiente OTP para completar tu cambio de contraseña. El OTP es valido en los siguientes 5 minutos</p>
+      <h2 style="background: #faafe7;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
+      <p style="font-size:0.9em;">Saludos,<br />CE FINA</p>
       <hr style="border:none;border-top:1px solid #eee" />
       <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-        <p>Koding 101 Inc</p>
-        <p>1600 Amphitheatre Parkway</p>
-        <p>California</p>
+        <p>Fina Garcia Inc</p>
+        <p>Camí Tovots</p>
+        <p>Barcelona - Manresa</p>
       </div>
     </div>
   </div>
